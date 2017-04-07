1fd169604233c9f5ff5710bd585df2f803fe1807
John S. Ryan <jtigger@infosysengr.com>
2017-04-06 19:14:16 -0700

    Decorated Me.

---
+++ src/main/java/io/pivotal/springroots/aspects/Minion.java
@@ -0,0 +1,19 @@
+package io.pivotal.springroots.aspects;
+
+public class Minion implements Me {
+	private final String name;
+
+	public Minion(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String name() {
+		return name;
+	}
+
+	@Override
+	public String greet(String other) {
+		return String.format("Hello, %s, it is I, %s!", other, name);
+	}
+}
