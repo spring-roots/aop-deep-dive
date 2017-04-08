1682a5ada460fcfd674ea36f63695ec7fb5673a8
John S. Ryan <jtigger@infosysengr.com>
2017-04-08 08:06:34 -0700

    Advice dynamically gets name of despicable.

--- src/main/java/io/pivotal/springroots/aspects/DespicableAdvice.java
+++ src/main/java/io/pivotal/springroots/aspects/DespicableAdvice.java
@@ -3,22 +3,31 @@ package io.pivotal.springroots.aspects;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
-public class DespicableAdvice implements MethodInterceptor {
-	private final String name;
-	public DespicableAdvice(String name) {
-		this.name = name;
-	}
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
+public class DespicableAdvice implements MethodInterceptor {
 	@Override
 	public Object invoke(MethodInvocation invocation) throws Throwable {
 		Object result = invocation.proceed();
-		if(result instanceof String) {
-			result = makeDespicable((String) result, name);
+		if (result instanceof String) {
+			result = makeDespicable((String) result, getNameFrom(invocation.getThis()));
 		}
 		return result;
 	}
 
 	private String makeDespicable(String string, String name) {
 		return string.replaceAll(name, "Despicable " + name);
+	}
+
+	private String getNameFrom(Object target) {
+		String name;
+		try {
+			Method nameGetter = target.getClass().getMethod("name");
+			name = (String) nameGetter.invoke(target);
+		} catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException excp) {
+			throw new RuntimeException(String.format("Unable to make %s despicable.", target.toString()), excp);
+		}
+		return name;
 	}
 }
